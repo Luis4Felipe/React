6c55426b019309249a233d2a39b9a6f8acc2b094
Luis4Felipe <161535240+Luis4Felipe@users.noreply.github.com>
2024-05-15 08:38:18 -0300

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,10 @@
+export const people = [{
+    id: 0,
+    name: 'Creola Katherine',
+    professor: 'matematica',
+    accomplisment: 'Calculadora espacial',
+    imageId: 'MK3eW3A'
+
+
+    
+}]
---
+++ src/utils.js
@@ -0,0 +1,20 @@
+export function getImageUrl(person, size = 's'){
+    return (
+        
+        'https://i.imgur.com/MK3eW3As.jpg' +
+        person.imageId +
+        size +
+        '.jpg'
+
+    );
+}
+
+export function getImageUrl(person){
+    return(
+        'https://i.imgur.com/MK3eW3As.jpg' +
+        person.imageId +
+        size +
+        '.jpg'
+
+    );
+}
